978d86951fa78b194671bab5c1d6a5acba3ccfeb
Sean Payerson <ssp6@hw.ac.uk>
2018-10-08 08:49:13 +0100

    Configures SASS support so that stories load correctly

--- stories/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import Home from '../src/web/components/Home';
-
-storiesOf('Home', module)
-  .add('As is', () => (
-    <Home />
-  ));
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import Home from '../src/web/components/Home';
+
+import '../src/web/styles/style.scss';
+// import '../src/web/styles/_bootstrap.scss';
 
 storiesOf('Home', module)
   .add('As is', () => (
