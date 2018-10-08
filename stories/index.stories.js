4c766f3cd705f52cced76e23b5299f6d3d1d34f8
Sean Payerson <ssp6@hw.ac.uk>
2018-10-08 09:06:32 +0100

    Adds STORYBOOK-README to explain how to run storybook and create stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import Home from '../src/web/components/Home';
 
 import '../src/web/styles/style.scss';
-// import '../src/web/styles/_bootstrap.scss';
+import '../src/web/styles/_bootstrap.scss';
 
 storiesOf('Home', module)
   .add('As is', () => (
