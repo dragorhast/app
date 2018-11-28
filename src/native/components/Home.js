7cb543d5ac33283a7c56a84bda754011bb8bccc6
Sean Payerson <ssp6@hw.ac.uk>
2018-11-28 15:58:03 +0000

    Commit

--- src/native/components/Home.js
+++ src/native/components/Home.js
@@ -5,22 +5,25 @@ import { Container, Content, Button, Text } from 'native-base';
 
 const Styles = StyleSheet.create({
   viewCenter: {
-    backgroundColor: 'blue',
-    color: 'green',
-    // justifyContent: 'center',
-    // alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
 // more props will be passed as functions later in development
 const Home = ({ logout }) => (
   <Container>
-    <Content>
-      <View styles={Styles.viewCenter}>
-        <Button primary large>
-          <Text>RENT NOW</Text>
-        </Button>
-      </View>
+    <Content contentContainerStyle={Styles.viewCenter}>
+      <Button primary large>
+        <Text>RENT NOW</Text>
+      </Button>
+      <Button primary large>
+        <Text>VIEW LOCATIONS</Text>
+      </Button>
+      <Button primary large>
+        <Text>VIEW BOOKINGS</Text>
+      </Button>
     </Content>
   </Container>
 );
