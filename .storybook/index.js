5d29716c32fd055e8fc6d2521535417642fe28b1
Sean Paterson <ssp6@hw.ac.uk>
2018-11-29 14:38:56 +0000

    Removes storybook and makes progress to reinstall it for react-native

---
+++ .storybook/index.js
@@ -0,0 +1,25 @@
+/**
+ * Follow this tutorial
+ *
+ * https://github.com/javidjamae/expo-with-storybook-howto
+ */
+
+import { AppRegistry } from 'react-native';
+import { getStorybookUI, configure } from '@storybook/react-native';
+
+import './rn-addons';
+
+// import stories
+configure(() => {
+  require('./stories');
+}, module);
+
+// Refer to https://github.com/storybooks/storybook/tree/master/app/react-native#start-command-parameters
+// To find allowed options for getStorybookUI
+const StorybookUIRoot = getStorybookUI({});
+
+// If you are using React Native vanilla write your app name here.
+// If you use Expo you can safely remove this line.
+// AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+
+export default StorybookUIRoot;
--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Root from './src/native/index';
 import configureStore from './src/redux/store/index';
 
+// storybook
+import StorybookUI from './.storybook';
+
 const { persistor, store } = configureStore();
 
 export default function App() {
-  return <Root store={store} persistor={persistor} />;
+  // return <Root store={store} persistor={persistor} />;
+  return StorybookUI;
 }
