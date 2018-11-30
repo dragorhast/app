23c2fbff805901c65cdf742bb751fd67303d45b1
Sean Paterson <ssp6@hw.ac.uk>
2018-11-30 11:14:19 +0000

    Centers the buttons on the home screen

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import Root from './src/native/index';
 import configureStore from './src/redux/store/index';
 
 // storybook
-import StorybookUI from './.storybook';
+// import StorybookUI from './.storybook';
 
 const { persistor, store } = configureStore();
 
 export default function App() {
-  // return <Root store={store} persistor={persistor} />;
-  return StorybookUI;
+  return <Root store={store} persistor={persistor} />;
+  // return StorybookUI;
 }
--- src/native/components/Home.js
+++ src/native/components/Home.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, StyleSheet } from 'react-native';
-import { Container, Content, Button, Text } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Container, Content, Body, Button, Text } from 'native-base';
 
 const Styles = StyleSheet.create({
   viewCenter: {
     flexDirection: 'column',
+    flex: 1,
+  },
+  blackBody: {
+    flexDirection: 'column',
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
   },
 });
 
 // more props will be passed as functions later in development
 const Home = ({ logout }) => (
   <Container>
+    {/* Content is the entire screen if flex is one */}
     <Content contentContainerStyle={Styles.viewCenter}>
-      <Button primary large>
-        <Text>RENT NOW</Text>
-      </Button>
-      <Button primary large>
-        <Text>VIEW LOCATIONS</Text>
-      </Button>
-      <Button primary large>
-        <Text>VIEW BOOKINGS</Text>
-      </Button>
+      <Body style={Styles.blackBody}>
+        <Button primary large style={Styles.buttonStyle}>
+          <Text>RENT NOW</Text>
+        </Button>
+        <Button bordered primary large>
+          <Text>VIEW LOCATIONS</Text>
+        </Button>
+      </Body>
     </Content>
   </Container>
 );
